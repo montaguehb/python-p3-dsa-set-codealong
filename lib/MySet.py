cecae3065fcbcbd862672c86392b881776085dbd
Historia Montague <historia.montague@outlook.com>
2023-05-26 10:37:43 -0600

    Finished Lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = None):
+        self.dictionary = {value: True for value in enumerable}
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
